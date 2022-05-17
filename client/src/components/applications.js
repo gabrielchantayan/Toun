986e3cfdf924030f6b97bd17f5dfb8664ebb4c48
gabrielchantayan <garnet638@gmail.com>
2022-05-17 01:16:37 -0400

    Populate bookmarks and apps

--- client/src/components/applications.js
+++ client/src/components/applications.js
@@ -1,29 +1,41 @@
 import * as locale from '../assets/js/localeManager.js'
+import * as appManager from '../assets/js/appManager.js'
+import * as appFactory from '../elements/appFactory.js'
 import React from 'react'
+
+
+
 
 export default class Applications extends React.Component {
 
     state = {
-        h3: []
+        h3: [],
+        appList: []
     }
 
     constructor() {
         super();
         locale.get('mainCategories', 'applications').then( (res) => {
             this.setState({ h3: res })
+        });
+
+        appManager.get('mainCategories', 'applications').then( (res) => {
+            appFactory.buildApps(res).then((res)=>{
+                this.setState({ appList: res })
+            })
         })
     }
 
     render() {
         return (
-         
-          <section id="apps">
+        
+        <section id="apps">
             <h3>{this.state.h3}</h3>
             <div id="apps_loop">
-              {/* {appFactory.buildApps()} */}
+                {this.state.appList}
             </div>
-          </section>
-          
+        </section>
+        
         )
     }
 }
